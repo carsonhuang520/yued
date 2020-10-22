9d3f46382afcb3ad784b50317a911ed8197459aa
carsonhuang <981347807@qq.com>
2020-10-22 21:36:01 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,11 @@ module.exports = {
   description: 'UI 框架',
   themeConfig: {
     sidebar: 'auto',
-    nav: [{ text: '主页', link: '' }],
+    nav: [
+      { text: '主页', link: '' },
+      { text: '文档', link: '/guide/' },
+      { text: '交流', link: 'https://google.com' },
+    ],
     sidebar: [
       {
         title: '入门',
